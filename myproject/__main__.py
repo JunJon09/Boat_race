5bd5df12b192a5fc353cf7473a806d02284efda9
JunJon09 <shibajunjun14@gmail.com>
2022-05-28 10:23:05 +0900

    ファイル構成

---
+++ myproject/__main__.py
@@ -0,0 +1,4 @@
+from scraping import test
+
+if __name__ == '__main__':
+    test()
---
+++ readers-repo-main/class/Peds.py
@@ -0,0 +1,60 @@
+class Peds:
+    def __init__(self, peds):
+        self.peds = peds
+        self.peds_e = pd.DataFrame() #after label encoding and transforming into category
+    
+    @classmethod
+    def read_pickle(cls, path_list):
+        df = pd.read_pickle(path_list[0])
+        for path in path_list[1:]:
+            df = update_data(df, pd.read_pickle(path))
+        return cls(df)
+    
+    @staticmethod
+    def scrape(horse_id_list):
+        """
+        血統データをスクレイピングする関数
+        Parameters:
+        ----------
+        horse_id_list : list
+            馬IDのリスト
+        Returns:
+        ----------
+        peds_df : pandas.DataFrame
+            全血統データをまとめてDataFrame型にしたもの
+        """
+
+        peds_dict = {}
+        for horse_id in tqdm(horse_id_list):
+            try:
+                url = "https://db.netkeiba.com/horse/ped/" + horse_id
+                df = pd.read_html(url)[0]
+
+                #重複を削除して1列のSeries型データに直す
+                generations = {}
+                for i in reversed(range(5)):
+                    generations[i] = df[i]
+                    df.drop([i], axis=1, inplace=True)
+                    df = df.drop_duplicates()
+                ped = pd.concat([generations[i] for i in range(5)]).rename(horse_id)
+
+                peds_dict[horse_id] = ped.reset_index(drop=True)
+                time.sleep(1)
+            except IndexError:
+                continue
+            except Exception as e:
+                print(e)
+                break
+            except:
+                break
+
+        #列名をpeds_0, ..., peds_61にする
+        peds_df = pd.concat([peds_dict[key] for key in peds_dict], axis=1).T.add_prefix('peds_')
+
+        return peds_df
+    
+    def encode(self):
+        df = self.peds.copy()
+        for column in df.columns:
+            df[column] = LabelEncoder().fit_transform(df[column].fillna('Na'))
+        self.peds_e = df.astype('category')
