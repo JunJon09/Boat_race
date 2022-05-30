5e9039b2814353c14e4ee6ed490a0a999ed37876
JunJon09 <shibajunjun14@gmail.com>
2022-05-30 17:01:54 +0900

    スクレピング01

--- myproject/__main__.py
+++ myproject/__main__.py
@@ -1,4 +1,15 @@
 from scraping import test
+import urllib.request
+import pandas as pd
+
 
 if __name__ == '__main__':
     test()
+
+    
+    f =  urllib.request.urlopen('https://kyotei.sakura.ne.jp/kako_kaijyo-09-20211222.html')
+    #https://kyotei.sakura.ne.jp/kako_kaijyo-09-20211222.html
+    decodeText = f.read().decode("utf-8")
+    print(decodeText)
+    
+     
